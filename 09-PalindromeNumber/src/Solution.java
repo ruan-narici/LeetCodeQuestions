61f68a50678c04574b3d88b7a54c33f714b7c8e9
Ruan Narici <ruannarici@hotmail.com>
2023-02-12 20:49:46 -0300

    Upload my solution for question 2

--- 09-PalindromeNumber/src/Solution.java
+++ 09-PalindromeNumber/src/Solution.java
@@ -3,7 +3,7 @@ class Solution {
     public boolean isPalindrome(int x) {
     	StringBuilder reverse = new StringBuilder().insert(0, x);
 // I created one reference for StringBuilder because I need to used the method reverse(). 
-//With the method, I achieved solved the questions. 
+//With the method, I achieved solved the question 9. 
     	if (reverse.toString().equals(reverse.reverse().toString())) {
     		return true;
     	} else {
